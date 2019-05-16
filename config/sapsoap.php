472e5b44ed75a2cf6ff37fa624637c55536ece27
curlykun <kurob1993@gmail.com>
2019-05-16 11:12:03 +0700

    perbaiki lg

--- config/sapsoap.php
+++ config/sapsoap.php
@@ -2,9 +2,9 @@
 
     return [
         'absence' => [
-            'url' => env('SI_ABSENCE','.../public/wsdl/SI_ABSENCE.WSDL')
+            'url' => env('SI_ABSENCE','../public/wsdl/SI_ABSENCE.WSDL')
         ],
         'absence_quota' => [
-            'url' => env('SI_ABSENCE_QUOTA','.../public/wsdl/SI_ABSENCE_QUOTA.WSDL')
+            'url' => env('SI_ABSENCE_QUOTA','../public/wsdl/SI_ABSENCE_QUOTA.WSDL')
         ]
     ];
