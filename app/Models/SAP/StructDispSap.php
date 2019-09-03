35c1f9608155ceead54e15b4bcef56fa5598999e
curlykun <kurob1993@gmail.com>
2019-09-03 13:29:37 +0700

    menampilkan old abbr

---
+++ app/Models/SAP/StructDispSap.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\SAP;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StructDispSap extends Model
+{
+    protected $table = 'structdisp_sap';
+    protected $primaryKey = null;
+    public $incrementing = false;
+    public $timestamps = false;
+}
