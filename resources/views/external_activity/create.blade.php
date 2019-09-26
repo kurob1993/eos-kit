4d35bc908b97b716753aad3f2cb2961b55e87963
curlykun <kurob1993@gmail.com>
2019-09-26 11:37:35 +0700

    perbaikan tampilan form activity

--- resources/views/external_activity/create.blade.php
+++ resources/views/external_activity/create.blade.php
@@ -36,20 +36,24 @@
           <div id="datepicker-range">
 
             <div class=" col-lg-5">
-              <input type="text" class="form-control text-center" id="start_date" 
-              name="start_date" value="{{ old('start_date') }}" placeholder='Pilih Tanggal mulai' readonly>
-              {!! $errors->first('start_date', ' <p class="text-danger help-block">:message</p>') !!}
-          
-              <div id="datepicker-range-start" class="datepicker-range"></div>
-            </div>
-
-            <div class=" col-lg-5">
-              <input type="text" class="form-control text-center is-invalid" id="end_date" 
-              name="end_date" value="{{ old('end_date') }}" placeholder='Pilih Tanggal berakhir' readonly>
-              {!! $errors->first('end_date', ' <p class="text-danger help-block">:message</p>') !!}
-
-              <div id="datepicker-range-end" class="datepicker-range"></div>
-            </div>
+                <label class="control-label col-sm-2" for="start_date">Mulai </label>
+                <input type="text" class="form-control text-center" id="start_date" 
+                name="start_date" value="{{ old('start_date') }}" placeholder='Pilih Tanggal mulai' 
+                style="background : white; color:black" readonly>
+                {!! $errors->first('start_date', ' <p class="text-danger help-block">:message</p>') !!}
+            
+                <div id="datepicker-range-start" class="datepicker-range"></div>
+              </div>
+  
+              <div class=" col-lg-5">
+                <label class="control-label col-sm-2" for="start_date">Berakhir </label>
+                <input type="text" class="form-control text-center is-invalid" id="end_date" 
+                name="end_date" value="{{ old('end_date') }}" placeholder='Pilih Tanggal berakhir' 
+                style="background : white; color:black" readonly>
+                {!! $errors->first('end_date', ' <p class="text-danger help-block">:message</p>') !!}
+  
+                <div id="datepicker-range-end" class="datepicker-range"></div>
+              </div>
 
           </div>
         </div>
--- resources/views/other_activity/_summary.blade.php
+++ resources/views/other_activity/_summary.blade.php
@@ -1,5 +1,6 @@
 <p class="pull-right">{{$when}}</p>
 <p class="m-b-0">{{$summary->jenis_kegiatan}}</p>
+<p class="m-b-0">{{$summary->keterangan}}</p>
 <p><small>{{$summary->start_date->format('d.m.Y')}}-{{$summary->end_date->format('d.m.Y')}}</small></p>
 @component('components._stage-description', [
     'class' => $summary->stage->class_description, 
