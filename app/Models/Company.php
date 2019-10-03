d7c9321b7d0079ed7f3e39df8fd3ae7e472550fd
alafghani <cppol08@gmail.com>
2019-10-03 07:59:11 +0700

    add relation company and company position

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    public function companyPosisition() 
+    {
+        return $this->hasMany('App\Models\CompanyPosisition');
+    }
 }
--- app/Models/CompanyPosisition.php
+++ app/Models/CompanyPosisition.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class CompanyPosisition extends Model
 {
+    public function companys() 
+    {
+        return $this->hasMany('App\Models\Company');
+    }
+
     public function preferdis()
     {
         return $this->hasMany('\App\Models\Preferdis');
