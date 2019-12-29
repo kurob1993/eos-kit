2f940dbd8c5907ad7c705dfbf40eae7ec5b78e1f
kurob <kurob1993@gmail.com>
2019-12-30 05:21:34 +0700

    as

--- resources/views/ski/kpi_leadership/_table.blade.php
+++ resources/views/ski/kpi_leadership/_table.blade.php
@@ -1,5 +1,5 @@
 <tr style="background-color: wheat">
-    <td class="text-center" style="background-color: white">IV</td>
+    <td class="text-center" style="background-color: white">V</td>
     <td><input type="text" name="kpi_leadership_aspek_penilaian[]" style="width: 100%" value="KPI Leadership" readonly autocomplete="off"></td>
     <td><input type="text" name="kpi_leadership_kode[]" style="width: 100%" value="" autocomplete="off"></td>
     <td><input type="text" name="kpi_leadership_sasran_prestasi_kerja[]" style="width: 100%" autocomplete="off"></td>
